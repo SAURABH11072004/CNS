4c8ca2d0fa6ca291f872f5cdbeb1d6cb310f534f
SAURABH MALI <sdmaniitian@gmail.com>
2025-08-08 10:54:30 +0530

    Update SimpleServer.java

--- Assignment4/SimpleServer.java
+++ Assignment4/SimpleServer.java
@@ -1,23 +1,22 @@
 import java.net.*;
 import java.io.*;
 
-public class SimpleServer3 {
+public class SimpleServer {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(1254);
         System.out.println("Server is running. Waiting for a client...");
         Socket socket = serverSocket.accept();
         System.out.println("Client connected!");
 
-        // Input and output streams
         DataInputStream in = new DataInputStream(socket.getInputStream());
         DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-        // Thread to receive messages
+        // Thread to receive messages from client
         Thread receiveThread = new Thread(() -> {
             try {
                 String message;
                 while (!(message = in.readUTF()).equalsIgnoreCase("exit")) {
-                    System.out.println("Client: " + message);
+                    System.out.println("Client: " + message);  // Print messages FROM client
                 }
                 System.out.println("Client ended the chat.");
                 System.exit(0);
@@ -26,11 +25,12 @@ public class SimpleServer3 {
             }
         });
 
-        // Thread to send messages
+        // Thread to send messages to client
         Thread sendThread = new Thread(() -> {
             try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
                 String message;
                 while (!(message = br.readLine()).equalsIgnoreCase("exit")) {
+                    System.out.println("Server: " + message);  // Print your own sent message
                     out.writeUTF(message);
                 }
                 out.writeUTF("exit");
@@ -39,7 +39,7 @@ public class SimpleServer3 {
             } catch (IOException e) {
                 System.out.println("Connection closed.");
             }
-        });  // <--- This closing parenthesis was missing
+        });
 
         receiveThread.start();
         sendThread.start();
